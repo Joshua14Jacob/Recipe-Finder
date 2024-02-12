dbf06d41e7936f37efa6d6f18e1f31eb6630f10e
Joshua Jacob <joshua14jacob@gmail.com>
2024-02-11 22:45:59 -0500

    added Dummy data

--- recipes_finder/views.py
+++ recipes_finder/views.py
@@ -1,8 +1,65 @@
 from django.shortcuts import render, HttpResponse
 
+recipes = [
+    {
+        'title': 'Pasta Carbonara',
+        'picture': 'pasta_carbonara.jpg',
+        'ingredients': [
+            'Spaghetti',
+            'Eggs',
+            'Bacon',
+            'Parmesan',
+            'Pepper'
+        ],
+        'instructions': [
+            'Boil the spaghetti',
+            'Fry the bacon',
+            'Mix the eggs and parmesan',
+            'Mix everything together'
+        ]
+    },
+    {
+        'title': 'Pasta Bolognese',
+        'picture': 'pasta_bolognese.jpg',
+        'ingredients': [
+            'Spaghetti',
+            'Beef',
+            'Tomato sauce',
+            'Onion',
+            'Garlic'
+        ],
+        'instructions': [
+            'Boil the spaghetti',
+            'Fry the beef',
+            'Fry the onion and garlic',
+            'Mix everything together'
+        ]
+    },
+    {
+        'title': 'Pasta Pesto',
+        'picture': 'pasta_pesto.jpg',
+        'ingredients': [
+            'Spaghetti',
+            'Basil',
+            'Pine nuts',
+            'Parmesan',
+            'Garlic'
+        ],
+        'instructions': [
+            'Boil the spaghetti',
+            'Blend the basil, pine nuts, parmesan, and garlic',
+            'Mix everything together'
+        ]
+    }
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Welcome to Recipe Finder!</h1>")
+    context = {
+        'recipes': recipes
+    }
+
+    return render(request, 'recipes_finder/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>This is a Recipe Finder app for users to find a recipe by entering some ingredients. </h1>")
+    return render(request, 'recipes_finder/about.html')
